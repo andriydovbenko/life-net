20907fe8316ec1ffadd09de9294a6602e440e6ff
andriydovbenko <andriydovbenko1@gmail.com>
2020-04-02 14:54:24 +0200

    init project

---
+++ src/main/java/org/lifenet/LifeNetApplication.java
@@ -0,0 +1,13 @@
+package org.lifenet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeNetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeNetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lifenet/LifeNetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lifenet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeNetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
