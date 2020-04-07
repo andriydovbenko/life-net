2bae4faf71e61239ea914af091cab21c1e3974c2
andriydovbenko <andriydovbenko1@gmail.com>
2020-04-07 14:17:31 +0200

    created controllers: 'MainController', 'MessageController'

--- src/main/java/org/lifenet/controller/MessageController.java
+++ src/main/java/org/lifenet/controller/MessageController.java
@@ -1,15 +1,57 @@
 package org.lifenet.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.annotation.JsonView;
+import org.lifenet.domain.Message;
+import org.lifenet.domain.View;
+import org.lifenet.repository.MessageRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("message")
 public class MessageController {
+    private final MessageRepository messageRepo;
+    private static int counter = 4;
+
+    @Autowired
+    public MessageController(MessageRepository messageRepo) {
+        this.messageRepo = messageRepo;
+    }
 
     @GetMapping
-    public String list(){
-        return "index";
+    public List<Message> getList() {
+        return messageRepo.findAll();
+    }
+
+    @GetMapping("{id}")
+    @JsonView(View.FullMessage.class)
+    public Message get(@PathVariable Message message) {
+        return message;
+    }
+
+    @PostMapping
+    public Message create(@RequestBody Message message) {
+        return messageRepo.save(message);
+    }
+
+    @PutMapping(value = "{id}")
+    public Message update(@PathVariable("id") Message messageFromDb,
+                          @RequestBody Message message) {
+        BeanUtils.copyProperties(message, messageFromDb, "id");
+
+        return messageRepo.save(messageFromDb);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable("id") Message message) {
+        messageRepo.delete(message);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public void deleteAll(@PathVariable("id") Message message) {
+        messageRepo.deleteAll();
     }
 }
